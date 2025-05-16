c458a23c162b264c4223273b398e4478f3effd52
James Conlin <jacon.dev@outlook.com>
2025-05-16 15:51:52 +0100

    Adding tests for performance to ensure that division and multiplication sit below 0.1 second

--- MathsChallenge/MathsProviderTests.cs
+++ MathsChallenge/MathsProviderTests.cs
@@ -1,12 +1,17 @@
+using System.Diagnostics;
+
 namespace MathsChallenge
 {
     public class Tests
     {
         private MathsProvider sut;
+        private Stopwatch timer;
+        const int MaxMilliseconds = 1000;
 
         [SetUp]
         public void Setup()
         {
+            timer = new Stopwatch();
             sut = new MathsProvider();
         }
 
@@ -17,9 +22,12 @@ namespace MathsChallenge
         [TestCase(-24, -16, -40)]
         public void When_calling_DoTheMath_with_addition_and_two_numbers_the_return_value_is_added_correctly(int firstNumber, int secondNumber, int expectedResult)
         {
+            timer.Start();
             var result = sut.DoTheMath(MathsAction.Addition, firstNumber, secondNumber);
+            timer.Stop();
 
             Assert.That(result, Is.EqualTo(expectedResult));
+            Assert.That(timer.ElapsedMilliseconds, Is.LessThanOrEqualTo(MaxMilliseconds));
         }
 
         [TestCase(5, 5, 0)]
@@ -29,9 +37,12 @@ namespace MathsChallenge
         [TestCase(-24, -16, -8)]
         public void When_calling_DoTheMath_with_subtraction_and_two_numbers_the_return_value_is_subtracted_correctly(int firstNumber, int secondNumber, int expectedResult)
         {
+            timer.Start();
             var result = sut.DoTheMath(MathsAction.Subtraction, firstNumber, secondNumber);
+            timer.Stop();
 
             Assert.That(result, Is.EqualTo(expectedResult));
+            Assert.That(timer.ElapsedMilliseconds, Is.LessThanOrEqualTo(MaxMilliseconds));
         }
 
         [TestCase(20, 5, 100)]
@@ -39,11 +50,16 @@ namespace MathsChallenge
         [TestCase(12, 6, 72)]
         [TestCase(10, 8, 80)]
         [TestCase(2, 30, 60)]
+        [TestCase(3, 536870911, 1610612733)]
+        [TestCase(2, 1073741823, 2147483646)]
         public void When_calling_DoTheMath_with_multiplication_and_two_numbers_the_return_value_is_subtracted_correctly(int firstNumber, int secondNumber, int expectedResult)
         {
+            timer.Start();
             var result = sut.DoTheMath(MathsAction.Multiplication, firstNumber, secondNumber);
+            timer.Stop();
 
             Assert.That(result, Is.EqualTo(expectedResult));
+            Assert.That(timer.ElapsedMilliseconds, Is.LessThanOrEqualTo(MaxMilliseconds));
         }
 
         [TestCase(25, 5, 5)]
@@ -51,11 +67,16 @@ namespace MathsChallenge
         [TestCase(12, 3, 4)]
         [TestCase(60, 20, 3)]
         [TestCase(30, 2, 15)]
+        [TestCase(1610612733, 3, 536870911)]
+        [TestCase(2147483646, 2, 1073741823)]
         public void When_calling_DoTheMath_with_division_and_two_numbers_the_return_value_is_subtracted_correctly(int firstNumber, int secondNumber, int expectedResult)
         {
+            timer.Start();
             var result = sut.DoTheMath(MathsAction.Division, firstNumber, secondNumber);
+            timer.Stop();
 
             Assert.That(result, Is.EqualTo(expectedResult));
+            Assert.That(timer.ElapsedMilliseconds, Is.LessThanOrEqualTo(MaxMilliseconds));
         }
     }
 }
