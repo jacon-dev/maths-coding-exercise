ace537f59bb7334866e4a6c09c66b032073f898c
James Conlin <jacon.dev@outlook.com>
2025-05-16 13:55:18 +0100

    Creating addition tests for challenge

--- MathsChallenge/MathsProviderTests.cs
+++ MathsChallenge/MathsProviderTests.cs
@@ -11,9 +11,15 @@ namespace MathsChallenge
         }
 
         [TestCase(5, 5, 10)]
+        [TestCase(13, 4, 17)]
+        [TestCase(12, 34, 46)]
+        [TestCase(-15, 8, -7)]
+        [TestCase(-24, -16, -40)]
         public void When_calling_DoTheMath_with_addition_and_two_numbers_the_return_value_is_added_correctly(int firstNumber, int secondNumber, int expectedResult)
         {
-            Assert.Fail();
+            var result = sut.DoTheMath(MathsAction.Addition, firstNumber, secondNumber);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
